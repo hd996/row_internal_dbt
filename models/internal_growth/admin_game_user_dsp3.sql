8d5d5e36be3754d6c13358d909deeafd65e724c9
hd996 <houdong111@gmail.com>
2022-08-09 20:09:40 +0800

    feat: sql

--- models/internal_growth/admin_game_user_dsp3.sql
+++ models/internal_growth/admin_game_user_dsp3.sql
@@ -1,7 +1,12 @@
 with source as (
-
-    select * from {{ ref('_airbyte_raw_admin_game_user_dsp3') }}
-
+  select * from {{ ref('_airbyte_raw_admin_game_user_dsp3') }}
 )
 
-select * from source
+final as (
+  select
+    Replace (JSON_EXTRACT(_airbyte_data, '$.game_id') , '"', '') as game_id,
+    Replace (JSON_EXTRACT(_airbyte_data, '$.user_id') ,'"', '') as user_id
+  from source
+)
+
+select * from final
